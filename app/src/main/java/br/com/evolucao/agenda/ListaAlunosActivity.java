0e45959d14fc0c700c70c4b17ab328f7b79bbc7b
Paulo Hunter <paulohunterd@gmail.com>
2017-11-03 16:44:13 -0300

    Deletando Alunos do Banco de Dados

--- app/src/main/java/br/com/evolucao/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/evolucao/agenda/ListaAlunosActivity.java
@@ -3,10 +3,15 @@ package br.com.evolucao.agenda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,10 +20,14 @@ import br.com.evolucao.agenda.modelo.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+    private ListView listaAlunos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_lista_alunos);
+        listaAlunos = (ListView) findViewById(R.id.Lista_alunos); /*Chamando list view do xml*/
+        carregaLista();
 
         //conexao com banco de dados
         //faz uma busca no banco para trazer alunos
@@ -27,23 +36,13 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         //----------------
         //Instanciar o DAO para conexao banco de dados
-
-        AlunoDAO dao = new AlunoDAO(this);
-        List<Aluno> alunos = dao.buscaAlunos();
-        dao.close();
-
-        ListView listaAlunos = (ListView) findViewById(R.id.Lista_alunos); /*Chamando list view do xml*/
-        ArrayAdapter<Aluno> adapter = new ArrayAdapter <Aluno> (this, android.R.layout.simple_list_item_1, alunos);
-        listaAlunos.setAdapter(adapter);
-
-
-
+        // por fim criar metodo carregaLista pelo refactory
 
 
         // TESTE PARA EBIBIR ALUNOS SEM BANCO -- String [] alunos = {"Daniel","Ronaldo", "Wanderson","Jeferson", "Felipe","Ronaldo", "Jeferson", "Felipe","Ronaldo", "Jeferson", "Felipe","Ronaldo", "Jeferson", "Felipe"};
-       // ListView listaAlunos = (ListView) findViewById(R.id.Lista_alunos); /*Chamando list view do xml*/
-      //  ArrayAdapter<String> adapter = new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, alunos);
-      //  listaAlunos.setAdapter(adapter);
+        // ListView listaAlunos = (ListView) findViewById(R.id.Lista_alunos); /*Chamando list view do xml*/
+        //  ArrayAdapter<String> adapter = new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, alunos);
+        //  listaAlunos.setAdapter(adapter);
 
         //definindo botao para navegar ate o formulario
         Button novoAluno = (Button) findViewById(R.id.novo_aluno);
@@ -54,5 +53,52 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 startActivity(intentVaiProFormulario);
             }
         });
+
+        //implementando opçao para excluir aluno
+        registerForContextMenu(listaAlunos);
     }
+
+    private void carregaLista() {
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
+
+        // comentado para criar a opção de apagar, colado no oncreate -- ListView listaAlunos = (ListView) findViewById(R.id.Lista_alunos); /*Chamando list view do xml*/
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregaLista();
+    }
+
+    //logo apos criar o register
+
+    @Override
+    //menu de contextoo, pressionar para aparecer o botão para deletas
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem deletar = menu.add("Deletar");
+        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+                //cod para deletar realmente o aluno
+
+                AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
+                dao.deleta(aluno);
+                dao.close();
+
+                carregaLista();
+
+                /*Toast.makeText(ListaAlunosActivity.this, "Deletar o Aluno " + aluno.getNome(), Toast.LENGTH_SHORT).show();*/
+                return false;
+            }
+        });
+    }
+
 }
+
